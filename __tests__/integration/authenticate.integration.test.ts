9231c50c5d5b636030bf75b05a6262c9aa78ea44
Marcio Angelo Matté <marcioamatte@gmail.com>
2022-05-07 13:52:08 -0300

    Update

--- __tests__/integration/authenticate.integration.test.ts
+++ __tests__/integration/authenticate.integration.test.ts
@@ -4,7 +4,8 @@ import { app } from '@/app/Server'
 describe('Integration test', () => {
   test('Authenticate login success', async () => {
     const user = { email: 'bauer@ctu.us', password: '123456789' }
-    const { status, body } = await request(app).post('/v1/auth').send(user).set('Accept', 'application/json')
+    const { status, body } = await request(app).post('/v1/auth')
+      .send(user).set('Accept', 'application/json')
     expect(status).toBe(200)
     expect(body.statusCode).toBe(200)
     expect(body).toHaveProperty('body')
@@ -12,7 +13,8 @@ describe('Integration test', () => {
 
   test('Authenticate login fail', async () => {
     const user = { email: 'bauer@ctu.us', password: '1234567899' }
-    const { status, body } = await request(app).post('/v1/auth').send(user).set('Accept', 'application/json')
+    const { status, body } = await request(app).post('/v1/auth')
+      .send(user).set('Accept', 'application/json')
     expect(status).toBe(400)
     expect(body.statusCode).toBe(400)
     expect(body.body).toHaveProperty('errorMessage')
